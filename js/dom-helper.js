2c46ef51401091cd5336891abfeb2af6e7972004
Zé Castro <a20351@alunos.ipca.pt>
2025-04-01 16:52:30 +0100

    feat(refactor): add new style

--- js/dom-helper.js
+++ js/dom-helper.js
@@ -14,26 +14,5 @@ function updateYear() {
 //// LINK CLICKS ////
 // Click on Website Logo
 document.getElementById('logo').addEventListener('click', () => {
-  window.location.href = 'index.html';
-});
-
-//// NAV BAR MOBILE ////
-// Open and close the mobile nav
-const menuBtn = document.getElementById('menu').querySelector('button');
-const navItems = document.querySelector('nav ul');
-
-menuBtn.addEventListener('click', () => {
-  // Check if the menu is open
-  if (navItems.classList.contains('menu-mobile')) {
-    // Run animation
-    navItems.style.animation = 'slideToLeft var(--slow-transition) forwards';
-    // Close the menu
-    setTimeout(() => {
-      navItems.style.animation = '';
-      navItems.classList.remove('menu-mobile');
-    }, 500);
-    return;
-  }
-
-  navItems.classList.toggle('menu-mobile');
+  window.location.href = '/';
 });
--- js/index.js
+++
@@ -1,2 +0,0 @@
-// This is the main javascript file of the project.
-// All of your new javascript code should be written here.
